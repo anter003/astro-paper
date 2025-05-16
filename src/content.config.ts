e4e00da4ecbcd60221a683b97316bb3a7881d884
anter <ppme7626@gmail.com>
2025-05-16 15:34:39 +0900

    base refactoring; routing

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,25 +2,31 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 import { SITE } from "@/config";
 
-export const BLOG_PATH = "src/data/blog";
+export const ARTICLE_PATH = "src/data/articles";
 
-const blog = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: `./${BLOG_PATH}` }),
-  schema: ({ image }) =>
+const projects = defineCollection({
+  loader: glob({ pattern : "**/s.json", base: `./${ARTICLE_PATH}` }),
+  schema: () => 
     z.object({
-      author: z.string().default(SITE.author),
-      pubDatetime: z.date(),
-      modDatetime: z.date().optional().nullable(),
+      seriesNo: z.number(),
       title: z.string(),
-      featured: z.boolean().optional(),
-      draft: z.boolean().optional(),
-      tags: z.array(z.string()).default(["others"]),
-      ogImage: image().or(z.string()).optional(),
-      description: z.string(),
-      canonicalURL: z.string().optional(),
-      hideEditPost: z.boolean().optional(),
-      timezone: z.string().optional(),
+      desc: z.string().optional()
     }),
 });
 
-export const collections = { blog };
+const docs = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: `./${ARTICLE_PATH}` }),
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().default(SITE.author),
+      pubDatetime: z.date().optional().nullable(),
+      title: z.string(),
+      series: z.number(),
+      draft: z.boolean().default(false),
+      tags: z.array(z.string()).default(["others"]),
+      ogImage: image().or(z.string()).optional(),
+      description: z.string(),
+    }),
+});
+
+export const collections = { docs, projects };
--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -1,4 +1,4 @@
-import { BLOG_PATH } from "@/content.config";
+import { ARITCLE_PATH } from "@/content.config";
 import { slugifyStr } from "./slugify";
 
 /**
@@ -14,14 +14,14 @@ export function getPath(
   includeBase = true
 ) {
   const pathSegments = filePath
-    ?.replace(BLOG_PATH, "")
+    ?.replace(ARITCLE_PATH, "")
     .split("/")
     .filter(path => path !== "") // remove empty string in the segments ["", "other-path"] <- empty string will be removed
     .filter(path => !path.startsWith("_")) // exclude directories start with underscore "_"
     .slice(0, -1) // remove the last segment_ file name_ since it's unnecessary
     .map(segment => slugifyStr(segment)); // slugify each segment path
 
-  const basePath = includeBase ? "/posts" : "";
+  const basePath = includeBase ? "/articles" : "";
 
   // Making sure `id` does not contain the directory
   const blogId = id.split("/");
